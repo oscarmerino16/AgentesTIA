383ab56d91ce3822d52f1a6849f1038414d12ad5
oscar <omc16@outlook.com>
2021-02-23 14:29:33 +0100

    a

--- src/Agentes/Main.java
+++ src/Agentes/Main.java
@@ -6,14 +6,8 @@ import Menu.Menu;
 
 public class Main {
 
-<<<<<<< HEAD
-	public static void verAgentes(Jefazo[] vAgentes) {
 
-		Scanner leer = new Scanner(System.in);
 
-		for (Jefazo j : vAgentes) {
-
-=======
 	public static void verAgentes(Agente[] vAgentes) {
 		
 		Scanner leer = new Scanner(System.in);
@@ -22,11 +16,11 @@ public class Main {
 			if (a!=null) {
 				System.out.println(a);
 			}
->>>>>>> main
+
 		}
 
 	}
-<<<<<<< HEAD
+
 	
 	public static void verAgentesDinero(Agente[] vAgentes) {
 		
@@ -80,30 +74,8 @@ public class Main {
 	}
 	
 	
-=======
 
 
->>>>>>> main
-	public static void main(String[] args) {
-		Scanner leer = new Scanner(System.in);
-		int opc = 0;
-		
-<<<<<<< HEAD
-		Menu.pintaMenu();
-		try {
-			opc=leer.nextInt();
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-		Menu.eleccionMenu(opc);
-/*
-		Jefazo vAgentes[] = new Jefazo[10];
-
-		vAgentes[0] = new Jefazo("Oscar", 20, "C/ Santa Clara n18", 5000, 2001);
-		vAgentes[1] = new Agente007("Marcos", 20, "C/ Castelseras n2", 3000, 250, 5);
-		vAgentes[2] = new AgenteEspionaje("Kasper", 20, "C/ El korral n34", 1500, 2);
-*/
-=======
 		Agente[] vAgentes = new Agente[10];
 
 		vAgentes[0] = new Jefazo("Oscar", 20, "C/ Santa Clara", 5000, 2011);
@@ -113,7 +85,7 @@ public class Main {
 		//verAgentes(vAgentes);
 		//verAgentesDinero(vAgentes);
 		
->>>>>>> main
+
 	}
 
 }
