d89a6c2109624be5a9a0ea78aac75afb155645db
oscar <valdealgorfa123>
2021-02-24 12:44:03 +0100

    a

--- src/Agentes/Main.java
+++ src/Agentes/Main.java
@@ -73,7 +73,7 @@ public class Main {
 		Agente[] vAgentes = new Agente[10];
 
 		vAgentes[0] = new Jefazo("Oscar", 20, "C/ Santa Clara", 5000, 2011);
-		vAgentes[1] = new Agente007("Marcos", 20, "C/ Castelseras n2", 3000, 250, 5);
+		vAgentes[1] = new Agente007("Marcos", 20, "C/ Castelseras n2", 3000, 250,0);
 		vAgentes[2] = new AgenteEspionaje("Kasper", 20, "C/ El korral n34", 1500, 2);
 		
 		Scanner leer = new Scanner(System.in);
